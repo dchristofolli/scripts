586e28382c850ace331dffba82bab29125abc7e7
dchristofolli <danielchristofolli@gmail.com>
2020-06-09 22:53:38 -0300

    node

--- node-install.sh
+++ node-install.sh
@@ -1,7 +1,9 @@
-wget https://nodejs.org/dist/v12.17.0/node-v12.17.0-linux-x64.tar.xz
+VERSION=v12.18.0
+DISTRO=linux-x64
+wget https://nodejs.org/dist/$VERSION/node-$VERSION-$DISTRO-$DISTRO.tar.xz
 mkdir -p /usr/local/lib/nodejs
-tar -xJvf node-v12.17.0-linux-x64.tar.xz -C /usr/local/lib/nodejs 
-export PATH=/usr/local/lib/nodejs/node-v12.17.0-linux-x64/bin:$PATH
+tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs
+export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 echo "export PATH=~/.npm-global/bin:$PATH < ~/.profile"
