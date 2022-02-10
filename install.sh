c78e2d0d843eb0661e75f9c23ed00166fed862df
Daniel Christofolli <danielchristofolli@gmail.com>
2022-02-10 10:33:13 -0300

    added sudo

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo Updating repositories..
-if ! apt update
+if ! sudo apt update
 then
-    echo "Não foi possível atualizar os repositórios. Verifique seu arquivo /etc/apt/sources.list"
+    echo "Não foi possível atualizar os repositórios. Verifique seu arquivo /etc/sudo apt/sources.list"
     exit 1
 fi
 echo “Atualização feita com sucesso”
 
 echo “Atualizando pacotes já instalados”
-if ! apt dist-upgrade -y
+if ! sudo apt dist-upgrade -y
 then
     echo “Não foi possível atualizar pacotes.”
     exit 1
@@ -17,47 +17,47 @@ fi
 echo “Atualização de pacotes feita com sucesso”
 
 # Git
-apt install git -y
+sudo apt install git -y
 
 # Zsh
-apt install zsh -y
+sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 
 # Gnome
-apt install gnome-session -y
+sudo apt install gnome-session -y
 
 # Docker
-apt -y install \
-    apt-transport-https \
+sudo apt -y install \
+    sudo apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
- add-apt-repository \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ add-sudo apt-repository \
  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
  $(lsb_release -cs) \
  stable"
-apt update
-apt -y install docker-ce \
+sudo apt update
+sudo apt -y install docker-ce \
     docker-ce-cli \
     containerd.io -y
-usermod -aG docker $USER
-systemctl start docker
-systemctl enable docker
+sudo usermod -aG docker $USER
+sudo systemctl start docker
+sudo systemctl enable docker
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # Google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb -y
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
 rm -f google-chrome-stable_current_amd64.deb*
 
 # Sublime text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt update
-apt install sublime-text -y
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo sudo apt-key add -
+echo "deb https://download.sublimetext.com/ sudo apt/stable/" | sudo tee /etc/sudo apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text -y
 
 # Sdkman
 curl -s "https://get.sdkman.io" | bash
@@ -86,9 +86,9 @@ sudo apt install code
 # Postman
 curl https://dl.pstmn.io/download/latest/linux -o postman.tar.gz
 tar zxvf postman.tar.gz
-mv Postman /opt
-ln -s /opt/Postman/Postman /usr/local/bin/postman
-cat <<EOF > /usr/share/applications/postman.desktop
+sudo mv Postman /opt
+sudo ln -s /opt/Postman/Postman /usr/local/bin/postman
+sudo cat <<EOF > /usr/share/applications/postman.desktop
 [Desktop Entry]
 Type=Application
 Name=Postman
@@ -100,23 +100,23 @@ EOF
 rm Postman-linux-x64-*.tar.gz
 
 # Spotify
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-apt update && apt install spotify-client -y
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/sudo apt/sources.list.d/spotify.list
+sudo apt update && sudo apt install spotify-client -y
 
 # VLC e Virtualbox
-apt install vlc virtualbox -y
+sudo apt install vlc virtualbox -y
 
 # Teams
 wget "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=linux&arch=x64&download=true&linuxArchiveType=deb" -O teams.deb
-apt install ./teams.deb
+sudo apt install ./teams.deb
 rm teams.deb
 
 # RocketChat
 wget "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/3.7.7/rocketchat_3.7.7_amd64.deb" -O rocketchat.deb
-apt install ./rocketchat.deb
+sudo apt install ./rocketchat.deb
 rm rocketchat.deb
 
 # Discord
 wget "https://dl.discordapp.net/apps/linux/0.0.16/discord-0.0.16.deb" -O discord.deb
-apt install ./discord.deb
+sudo apt install ./discord.deb
